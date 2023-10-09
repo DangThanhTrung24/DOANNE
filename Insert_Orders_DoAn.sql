dbf5bc1e972e107aae225a68e526beabac942b9c
DangThanhTrung24 <dangthanhtrung57@gmail.com>
2023-10-09 21:16:28 +0700

    delete

--- Insert_Orders_DoAn.sql
+++
@@ -1,9 +0,0 @@
-select * from orders
-SELECT * FROM [ADDRESS]
-INSERT INTO Orders VALUES
-(N'111863', 7, 2, NULL, 1, CAST(N'2023-09-25' AS Date), N'1', N'0', N''),
-(N'565067', 10, 3, 1, 3, CAST(N'2023-09-25' AS Date), N'1', N'1', N''),
-(N'129524', 13, 4, NULL, 5, CAST(N'2023-09-25' AS Date), N'1', N'2', N''),
-(N'638499', 15, 5, 2, 4, CAST(N'2023-09-25' AS Date), N'1', N'3', N''),
-(N'957074', 21, 6, NULL, 3, CAST(N'2023-09-25' AS Date), N'0', N'2', N''),
-(N'245535', 2, 7, 3, 2, CAST(N'2023-09-25' AS Date), N'0', N'3', N'')
