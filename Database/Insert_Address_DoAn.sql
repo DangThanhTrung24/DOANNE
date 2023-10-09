7e1f39b29b4144f83d0d860aa6621a9e51464048
DangThanhTrung24 <dangthanhtrung57@gmail.com>
2023-10-09 21:18:23 +0700

    database

---
+++ Database/Insert_Address_DoAn.sql
@@ -0,0 +1,9 @@
+﻿INSERT INTO [Address] VALUES
+	(N'Đặng Thành Trung', N'0889641810', N'Hồ Chí Minh', N'Tân Bình', N'15', N'600 Phạm Văn Bạch, phường 15, quận Tân Bình, TP.Hồ Chí Minh', 1),
+	(N'Nguyễn Đại Lộc', N'0889641811', N'Hồ Chí Minh', N'Gò Vấp', N'14', N'100 Lê Đức Thọ, phường 14, quận Gò Vấp, TP.Hồ Chí Minh', 2),
+	(N'Vũ Nhật Tân', N'0889641812', N'Hồ Chí Minh', N'Quận 3', N'2', N'123 Văn Cao, phường 2, quận 3, TP.Hồ Chí Minh', 3),
+	(N'Nguyễn Trần Nhật Anh', N'0889641813', N'Hồ Chí Minh', N'Thủ Đức', N'Hiệp Bình Phước', N'123 quốc lộ 13, phường Hiệp Bình Phước, quận Thủ Đức, TP.Hồ Chí Minh', 4),
+	(N'Nguyễn Quang Sáng', N'0889641814', N'Hồ Chí Minh', N'Tân Bình', N'P15', N'100 Phạm Văn Bạch, phường 15, quận Tân Bình, TP.Hồ Chí Minh', 5),
+
+	(N'Phạm Thị Nở', N'0889641815', N'Hồ Chí Minh', N'Tân Bình', N'15', N'Tân Sơn, phường 15, quận Tân Bình, TP.Hồ Chí Minh', 6),
+	(N'Nguyễn Chí Phèo', N'0889641816', N'Hồ Chí Minh', N'Tân Bình', N'15', N'Nguyễn Phúc Chu, phường 15, quận Tân Bình, TP.Hồ Chí Minh', 7);
